f3af2e7bbe81f6feac75f1ac57929e7c7e24d5e1
ScarryP <49690013+ScarryP@users.noreply.github.com>
2022-08-23 22:15:22 +0200

    update camera block style

--- src/components/blocco/index.js
+++ src/components/blocco/index.js
@@ -1,21 +1,40 @@
-import './style.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faVideo, faPlus } from '@fortawesome/free-solid-svg-icons'
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPenToSquare,
+  faVideo,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Blocco = () => {
-    return (
-        <div className='blocco'>
-            <p>Name <br />
-                IP <br />
-                Status <br/>
-            </p>
-
-            <FontAwesomeIcon icon={faVideo} className="videocamera" />
-
-            <a href='edit'><FontAwesomeIcon icon={faPenToSquare} className="penToSquare" /></a>
-            <a href='details'><FontAwesomeIcon icon={faPlus} className="plus" /></a>
+  return (
+    <div className="blocco">
+      <div className="row">
+        <div>
+          <p>
+            Name <br />
+            IP <br />
+            Status <br />
+          </p>
         </div>
-    );
-}
+        <div>
+          <a href="edit">
+            <FontAwesomeIcon icon={faPenToSquare} className="penToSquare" />
+          </a>
+        </div>
+      </div>
+      <div className="row">
+        <div>
+          <FontAwesomeIcon icon={faVideo} className="videocamera" />
+        </div>
+        <div>
+          <a href="details">
+            <FontAwesomeIcon icon={faPlus} className="plus" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Blocco;
